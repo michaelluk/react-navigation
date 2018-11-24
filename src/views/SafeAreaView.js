2f6f4a09859b70cde19baf935458660c9ecfb36a
Michael Luk <michaelkaka123@gmail.com>
2018-11-24 11:26:18 +0800

    quick fix for iphone xr and xr max

--- src/views/SafeAreaView.js
+++ src/views/SafeAreaView.js
@@ -14,6 +14,8 @@ import withOrientation from './withOrientation';
 // See https://mydevice.io/devices/ for device dimensions
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
+const XSMAX_WIDTH = 414;
+const XSMAX_HEIGHT = 896;
 const PAD_WIDTH = 768;
 const PAD_HEIGHT = 1024;
 
@@ -32,7 +34,9 @@ const isIPhoneX = (() => {
   return (
     Platform.OS === 'ios' &&
     ((D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) ||
-      (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT))
+        (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT)) ||
+    ((D_HEIGHT === XSMAX_HEIGHT && D_WIDTH === XSMAX_WIDTH) ||
+        (D_HEIGHT === XSMAX_WIDTH && D_WIDTH === XSMAX_HEIGHT))
   );
 })();
 
